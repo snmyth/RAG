f6fda09d5e45646a5b20b485962151f91773cd3c
snmyth <pawarsanmit1@gmail.com>
2026-08-08 18:19:18 +0530

    Initial Working of RAG

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from pypdf import PdfReader 
 from sentence_transformers import SentenceTransformer
+import chromadb
 
 reader = PdfReader("test.pdf")
 
@@ -20,4 +21,21 @@ model = SentenceTransformer('all-MiniLM-L6-v2')
 embeddings = model.encode(chunks)
 
 print(len(embeddings))
-print()
+print(len(embeddings[0]))
+
+client = chromadb.Client()
+
+collection = client.create_collection(name="my_pdf")
+
+collection.add(
+    documents = chunks,
+    embeddings = embeddings,
+    ids = [str(i) for i in range(len(chunks))]
+)
+
+
+question = "What is Battery life"
+enc = model.encode([question])
+result =collection.query(query_embeddings = enc, n_results = 2)
+
+print(result)
